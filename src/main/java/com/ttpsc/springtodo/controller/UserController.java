26fa7e73c4824ec0e841368e98849da331a9c5cb
PawelKrol23 <p.krol18a@gmail.com>
2023-11-12 00:13:58 +0100

    Added UserDTO, updated user creation page

--- src/main/java/com/ttpsc/springtodo/controller/UserController.java
+++ src/main/java/com/ttpsc/springtodo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ttpsc.springtodo.controller;
 
+import com.ttpsc.springtodo.model.UserDTO;
 import com.ttpsc.springtodo.model.UserEntity;
 import com.ttpsc.springtodo.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -16,8 +18,14 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/user")
-    public String createUser(Model model) {
+    public String createUserForm(Model model) {
         model.addAttribute("user", new UserEntity());
+        return "user/create";
+    }
+
+    @PostMapping("/user")
+    public String createUser(Model model) {
+        model.addAttribute("user", new UserDTO());
         return "user/create";
     }
 
---
+++ src/main/java/com/ttpsc/springtodo/model/UserDTO.java
@@ -0,0 +1,15 @@
+package com.ttpsc.springtodo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String username;
+    private String password;
+}
