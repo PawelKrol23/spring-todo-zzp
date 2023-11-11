d63f07669c3e8c4f3647cfdb2c223bc93dfbad82
PawelKrol23 <p.krol18a@gmail.com>
2023-11-11 23:11:45 +0100

    Added commons html file with navbar, added create new user template, updated security

---
+++ src/main/java/com/ttpsc/springtodo/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ttpsc.springtodo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/user").hasAnyAuthority("admin")
+                .anyRequest().authenticated();
+        return http.build();
+    }
+
+
+}
--- src/main/java/com/ttpsc/springtodo/controller/UserController.java
+++ src/main/java/com/ttpsc/springtodo/controller/UserController.java
@@ -3,21 +3,22 @@ package com.ttpsc.springtodo.controller;
 import com.ttpsc.springtodo.model.User;
 import com.ttpsc.springtodo.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/user")
-    public List<User> getAllUsers() {
-        return userService.getUsers();
+    public String getAllUsers(Model model) {
+        model.addAttribute("user", new User());
+        return "user/create";
     }
 
     @GetMapping("/user/{id}")
