c32ff4aa3e2c8d01ae1868ea499637bb69094845
PawelKrol23 <p.krol18a@gmail.com>
2023-11-13 12:12:48 +0100

    Added status validations

--- src/main/java/com/ttpsc/springtodo/model/Status.java
+++ src/main/java/com/ttpsc/springtodo/model/Status.java
@@ -1,6 +1,7 @@
 package com.ttpsc.springtodo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
@@ -14,7 +15,11 @@ public class Status {
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "status_id_seq")
     @GenericGenerator(name = "status_id_seq", strategy = "increment")
     private Long id;
+
+    @Size(min = 2, max = 20)
     private String name;
+
+    @Size(min = 2, max = 20)
     private String displayname;
 
     @ManyToOne
